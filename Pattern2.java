53ca5974edaa6c2ed01fb95e54cdbdf959c4a659
Kalyanprabhu1 <115627346+Kalyanprabhu1@users.noreply.github.com>
2022-10-25 19:52:33 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,23 @@
+package king.java;
+
+public class Pattern2 {
+        public static void main(String[] args)
+		{
+			int n=13;
+			for(int i=0;i<n;i++)
+			{
+				for(int j=0;j<n;j++)
+				{
+					if(i-j>=(n-1)/2||i+j>=((n-1)/2)+(n-1))
+					   {
+					     System.out.print("@");
+					   }
+					else
+					   {
+					     System.out.print(" ");
+	       		       }
+				}
+	                     System.out.println(); 
+			}
+		}
+}
---
+++ Pattern3.java
@@ -0,0 +1,23 @@
+package king.java;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+	        int n=15;
+			for(int i=0;i<n;i++)
+			{
+				for(int j=0;j<n;j++)
+				{
+					if(i+j<=(n-1)/2||i-j>=(n-1)/2||i==0&&j>(n-1)/2||i==(n-1)&&j>(n-1)/2)
+					   {
+					     System.out.print("&");
+					   }
+					else
+					   {
+					     System.out.print(" ");
+					   }
+				}
+				        System.out.println();
+	        }
+	}
+}
